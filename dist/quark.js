b9f388e6f7ed7377632a56cee213866c0445b015
admin <fllopez@bpba.com.ar>
2015-09-16 14:54:15 -0300

    Ajustes en routing para que funcione con multiples location

--- dist/quark.js
+++ dist/quark.js
@@ -820,143 +820,200 @@ ko.bindingHandlers.page = {
 }
 
 //crossroads, hasher
-
 function QuarkRouter() {
     var self = this;
 
-    var conf = {};
+    this.current = ko.observable();
+    this.configuration = [];
 
     function RoutingConfig() {
-        var self = this;
-        var currentPage;
-        var currentName;
+        // Self
+        var routingConfig = this;
 
-        this.on = function(page, name) {
-            if (!$$.isString(page)) {
-                throw 'The page must be an string with the name of the page without extension.';
+        // Router Configuration
+        routingConfig.configuration = {};
+
+        // Location's name
+        var currentLocationName;
+
+        // Adds a location to the route. The routes applied to it using the .when method are parsed if the location.pathname
+        // matches the specified pattern
+        this.on = function(locationPattern, name) {
+            if (!$$.isString(locationPattern)) {
+                throw 'The page must be an string with the pattern of the pathname to match.';
             }
 
             if (!$$.isDefined(name)) {
                 throw 'Must define a name for the routes on the page.';
             }
 
-            conf[page] = {};
-            currentPage = page;
-            currentName = name;
+            // Initialize the location configuration
+            routingConfig.configuration[name] = {
+                pattern: locationPattern,
+                routes: {}
+            };
 
-            return self;
+            // Sets the current location and current so it can be used by the .when method in chained calls.
+            currentLocationName = name;
+
+            // Returns itself so config methods are chainable.
+            return routingConfig;
         }
 
-        this.when = function(url, name, config) {
-            // If name and config are not defined we assume that the parameter is the config
-            if (!$$.isDefined(name) && !$$.isDefined(config)) {
-                config = url;
+        // Adds a route to the last location specified with .on. The url is a pattern to match on the hash, the
+        // name parameter is the name of the route, and the components parameter is an object with each property being the name of a placeholder
+        // and it's value the component that must be binded on it.
+        this.when = function(url, name, components) {
+            // If only one parameter is specified we assume that its the components parameter
+            if (!$$.isDefined(name) && !$$.isDefined(components)) {
+                components = url;
                 name = '';
                 url = 'Any';
-            } else if (!$$.isDefined(config)) {
-                // If only two parameters are defined we assume that are the url and config
-                config = name;
-                name = '';
+            } else if (!$$.isDefined(components) || !$$.isDefined(name)) {
+                throw 'Must define the url, name and components parameters'
             }
 
-            if (!$$.isDefined(url)) {
-                throw 'You must define at least the config.'
+            // If .on was not called previously
+            if (!$$.isDefined(currentLocationName)) {
+                throw 'You must define the location to where this route applies using the .on method before calling .when.';
             }
 
-            if (!$$.isDefined(currentPage)) {
-                throw 'You must define the main page using the .on method before calling .when.';
-            }
+            // Forms full name (location name/route name)
+            var fullName = currentLocationName + '/' + name;
 
-            config.name = currentName + '/' + name;
-            conf[currentPage][url] = config;
+            // Loads the configuration
+            routingConfig.configuration[currentLocationName]['routes'][name] = {
+                url: url,
+                components: components,
+                name: fullName
+            };
 
-
-            return self;
+            // Returns itself so config methods are chainable.
+            return routingConfig;
         }
     }
 
-    function Router() {
-        var self = this;
+    function Route(router, name, locationPattern, url, components) {
+        var routeObject = this;
 
-        var started = false;
-        var routes = [];
+        var csRoute = router.addRoute(url, function(requestParams) {
+            self.current({
+                route: routeObject,
+                location: location.pathname,
+                params: requestParams
+            });
+        });
 
-        this.current = ko.observable();
-
-        function start() {
-            var prev;
-            var cross;
-            for (var path in conf) {
-                prev = cross;
-                cross = crossroads.create();
-                cross.normalizeFn = cross.NORM_AS_OBJECT;
-
-                for (var url in conf[path]) {
-                    if (url != 'Any') {
-                        var config = conf[path][url];
-
-                        function Route() {
-                            var r = this;
-                            this.file = path;
-                            this.url = url;
-                            this.name = config.name;
-                            this.components = ko.utils.extend(config, conf[path]['Any']);
-                            this.event = cross.addRoute(url, function(requestParams) {
-                                var exp = RegExp(path);
-                                if (location.pathname.match(exp)) {
-                                    r.params = requestParams;
-                                    self.current(r);
-                                }
-                            });
-                            this.interpolate = function(values) {
-                                return r.event.interpolate(values);
-                            }
-                        }
-
-                        if (!prev) {
-                            crossroads.pipe(cross);
-                        } else {
-                            prev.pipe(cross);
-                        }
-
-                        routes.push(new Route());
-                    }
-                }
-            }
-
-            started = true;
-        }
+        this.name = name;
+        this.locationPattern = locationPattern;
+        this.url = url;
+        this.components = components;
 
         this.parse = function(url) {
-            if (!started) start();
-            return crossroads.parse(url);
+            router.parse(url);
         }
 
-        this.get = function(name) {
-            for (var i = 0; i < routes.length; i++) {
-                var route = routes[i];
-                if (route.name && route.name == name) {
-                    return route;
+        this.interpolate = function(values) {
+            return csRoute.interpolate(values);
+        }
+    }
+
+    this.routes = function() {
+        return new RoutingConfig();
+    }
+
+    this.configure = function(routingConfig) {
+        // For each location configurated
+        for (var locationName in routingConfig.configuration) {
+            // Get this location config and the pattern
+            var locationConfig = routingConfig.configuration[locationName];
+            var locationPattern = routingConfig.configuration[locationName].pattern;
+
+            // Create a new crossroads router
+            var newRouter = crossroads.create();
+            newRouter.normalizeFn = crossroads.NORM_AS_OBJECT;
+
+            // Adds the router to the location config
+            locationConfig.router = newRouter;
+
+            // For each url configured for this location
+            for (var routeName in locationConfig.routes) {
+                if (routeName !== '') {
+                    var components;
+
+                    // Gets the route configuration
+                    var routeConfig = locationConfig.routes[routeName];
+
+                    components = routeConfig.components;
+
+                    if ($$.isDefined(locationConfig.routes[''])) {
+                        $.extend(components, locationConfig.routes[''].components);
+                    }
+
+                    // Creates the new router
+                    var newRoute = new Route(newRouter, routeConfig.name, locationPattern, routeConfig.url, components);
+
+                    routeConfig.route = newRoute;
                 }
             }
         }
 
-        this.link = function(name, config) {
-            for (var i = 0; i < routes.length; i++) {
-                var route = routes[i];
-                if (route.name && route.name == name) {
-                    return route.location + '#' + route.interpolate(config);
-                }
+        self.configuration = routingConfig.configuration;
+    }
+
+    this.parse = function(location, url) {
+        // If only one parameter is specified we assume that is the hash, and the location must be taken from the
+        // window location object.
+        if (!$$.isDefined(url)) {
+            url = location;
+            location = window.location.pathname;
+        }
+
+        for (var locationName in self.configuration) {
+            var locationConfig = self.configuration[locationName];
+
+            var exp = RegExp(locationConfig.pattern);
+            if (location.match(exp)) {
+                return locationConfig.router.parse(url);
             }
         }
     }
 
-    this.config = new RoutingConfig();
-    this.route = new Router();
+    this.getRoute = function(name) {
+        var parts = name.split('/');
 
-    this.followLocation = function() {
+        if (parts.length != 2) {
+            throw new 'You must specifiy route name in the form pageName/routeName';
+        }
+
+        if (self.configuration[parts[0]] && self.configuration[parts[0]]['routes'][parts[1]]) {
+            return self.configuration[parts[0]]['routes'][parts[1]]['route'];
+        }
+    }
+
+    this.hash = function(name, config) {
+        var route = self.getRoute(name);
+
+        if (route) {
+            return route.interpolate(config);
+        }
+    }
+
+    this.link = function(name, config, location) {
+        var route = self.getRoute(name);
+
+        if (!location) {
+            location = route.locationPattern;
+        }
+
+        if (route) {
+            return location + '#' + route.interpolate(config);
+        }
+    }
+
+    this.activateHasher = function() {
         function parseHash(newHash, oldHash) {
-            self.route.parse(newHash);
+            self.parse(newHash);
         }
 
         hasher.initialized.add(parseHash);
--- test/modules/routing.js
+++ test/modules/routing.js
@@ -1,49 +1,57 @@
 define(['quark'], function($$) {
     describe('Core - Behaviours Tests', function() {
-        $$.routing.config
-            .on('/nomatch/*', 'nomatch')
-                .when('/prueba3/{param1}/{param2}', 'three', {
-                    test: 'Controller3'
-                })
-            .on('/*', 'test')
-                .when({
-                    top: 'TopController'
-                })
-                .when('/prueba1/{param1}/{param2}', 'one', {
-                    test: 'Controller1'
-                })
-                .when('/prueba2/{param3}', 'two', {
-                    holder1: 'Controller2',
-                    holder2: 'Controller3'
-                });
+        $$.routing.configure(
+            $$.routing.routes()
+                .on('/nomatch', 'nomatch')
+                    .when('/prueba3/{param1}/{param2}', 'three', {
+                        test: 'Controller3'
+                    })
+                .on('/*', 'test')
+                    .when({
+                        top: 'TopController'
+                    })
+                    .when('/prueba1/{param1}/{param2}', 'one', {
+                        test: 'Controller1'
+                    })
+                    .when('/prueba2/{param3}', 'two', {
+                        holder1: 'Controller2',
+                        holder2: 'Controller3'
+                    })
+        );
 
         it ('Must Parse Route correctly and combine with \"any\" controllers', function() {
-            $$.routing.route.parse('/prueba1/Hola/Mundo');
-            var route = $$.routing.route.current();
+            $$.routing.parse('/prueba1/Hola/Mundo');
+            var current = $$.routing.current();
 
-            expect(route.file).toBe('/*');
-            expect($$.isDefined(route.components)).toBe(true);
-            expect($$.isDefined(route.components.top)).toBe(true);
-            expect($$.isDefined(route.components.test)).toBe(true);
-            expect(route.params.param1).toBe('Hola');
-            expect(route.params.param2).toBe('Mundo');
-            expect(route.url).toBe('/prueba1/{param1}/{param2}');
+            expect(current.route.locationPattern).toBe('/*');
+            expect($$.isDefined(current.route.components)).toBe(true);
+            expect(current.route.components.top).toBe('TopController');
+            expect(current.route.components.test).toBe('Controller1');
+            expect(current.params.param1).toBe('Hola');
+            expect(current.params.param2).toBe('Mundo');
         });
 
         it ('Must Parse Route 2 correctly and combine with \"any\" controllers', function() {
-            $$.routing.route.parse('/prueba2/Hola/');
-            var route = $$.routing.route.current();
+            $$.routing.parse('/prueba2/Hola/');
+            var current = $$.routing.current();
 
-            expect(route.file).toBe('/*');
-            expect($$.isDefined(route.components)).toBe(true);
-            expect($$.isDefined(route.components.top)).toBe(true);
-            expect($$.isDefined(route.components.holder1)).toBe(true);
-            expect($$.isDefined(route.components.holder2)).toBe(true);
-            expect(route.url).toBe('/prueba2/{param3}');
+            expect(current.route.locationPattern).toBe('/*');
+            expect($$.isDefined(current.route.components)).toBe(true);
+            expect(current.route.components.top).toBe('TopController');
+            expect(current.route.components.holder1).toBe('Controller2');
+            expect(current.route.components.holder2).toBe('Controller3');
+            expect(current.route.url).toBe('/prueba2/{param3}');
         });
 
+
         it ('Must Get Route 1', function() {
-            expect($$.routing.route.get('test/one').name).toBe('test/one');
+            var route = $$.routing.getRoute('test/one');
+            expect(route.name).toBe('test/one');
+        });
+
+        it ('Must Generate a valid link to route', function() {
+            var link = $$.routing.link('test/one', { param1: 'Adios', param2: 'Mundo Cruel' }, '/test');
+            expect(link).toBe('/test#/prueba1/Adios/Mundo Cruel');
         });
     });
 
