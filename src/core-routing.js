b7e79379e0f6e9e7142679d5e753bb6d263b992f
fran150 <franllopez@yahoo.com.ar>
2015-10-30 00:31:21 -0300

    Rediseño del router

--- src/core-routing.js
+++ src/core-routing.js
@@ -5,6 +5,28 @@ function QuarkRouter() {
     this.current = ko.observable();
     this.configuration = {};
 
+    this.locationFinders = [];
+
+    this.locationFinders.push(function(currentLocation, callback) {
+        var path;
+
+        if (!currentLocation) {
+            path = window.location.pathname;
+        } else {
+            path = currentLocation;
+        }
+
+        for (var locationName in self.configuration) {
+            var location = self.configuration[locationName];
+
+            var exp = RegExp(location.config);
+
+            if (path.match(exp)) {
+                callback(location);
+            }
+        }
+    });
+
     function RoutingConfig() {
         // Self
         var routingConfig = this;
@@ -17,18 +39,14 @@ function QuarkRouter() {
 
         // Adds a location to the route. The routes applied to it using the .when method are parsed if the location.pathname
         // matches the specified pattern
-        this.on = function(locationPattern, name) {
-            if (!$$.isString(locationPattern)) {
-                throw 'The page must be an string with the pattern of the pathname to match.';
-            }
-
+        this.on = function(name, config) {
             if (!$$.isDefined(name)) {
                 throw 'Must define a name for the routes on the page.';
             }
 
             // Initialize the location configuration
             routingConfig.configuration[name] = {
-                pattern: locationPattern,
+                config: config,
                 routes: {}
             };
 
@@ -74,148 +92,168 @@ function QuarkRouter() {
         }
     }
 
-    function Route(router, name, fullName, locationPattern, hash, components, controller) {
+    function Route(router, name, fullName, locationConfig, hash, components, controller) {
         var routeObject = this;
 
+        // Add route in crossroad
         var csRoute = router.addRoute(hash, function(requestParams) {
+            // If the current route has a controller defined and the controller has a leaving method call it to allow
+            // controller cleanup, if controller result is false do not reroute
             if (self.current() && self.current().controller && self.current().controller.leaving) {
                 if (self.current().controller.leaving() === false) {
                     return;
                 }
             }
 
-            if ($$.isString(controller)) {
-                require([controller], function(controllerObject) {
-                    var routeController = $$.isFunction(controllerObject) ? new controllerObject : controllerObject;
-
-                    self.current({
-                        route: routeObject,
-                        location: location.pathname,
-                        params: requestParams,
-                        controller: routeController
-                    });
-
-                    if (routeController && routeController.show) {
-                        routeController.show();
-                    }
-
-                    self.routed.dispatch();
+            // Changes route setting the specified controller
+            function changeCurrent(routeController) {
+                // Change the current route
+                self.current({
+                    route: routeObject,
+                    params: requestParams,
+                    controller: routeController
                 });
-            } else {
-                var loadedCallback = function(controllerObject) {
-                    self.current({
-                        route: routeObject,
-                        location: location.pathname,
-                        params: requestParams,
-                        controller: controllerObject
-                    });
 
-                    if (controllerObject && controllerObject.show) {
-                        controllerObject.show();
-                    }
-
-                    self.routed.dispatch();
+                // If the controller is defined and has a show method invoke it
+                if (routeController && routeController.show) {
+                    routeController.show();
                 }
 
+                // Dispatch the routed signal
+                self.routed.dispatch();
+            }
+
+            // If the controller is a string then assume its a js module name
+            if ($$.isString(controller)) {
+                // Require the controller file
+                require([controller], function(controllerObject) {
+                    // If the module returns a constructor create an object, if not use it as is
+                    var routeController = $$.isFunction(controllerObject) ? new controllerObject : controllerObject;
+
+                    // Change current route using the loaded controller
+                    changeCurrent(routeController);
+                });
+            } else {
+                // If controller is a function, the function must create the controller object and
+                // invoke the callback passed as firt parameter
                 if ($$.isFunction(controller)) {
-                    controller(loadedCallback);
+                    controller(changeCurrent);
                 } else {
-                    loadedCallback(controller);
+                    // If the controller is not an string nor function then use it as specified
+                    changeCurrent(controller);
                 }
             }
         });
 
+        // Name of the route
         this.name = name;
+        // Full name of the route (including location)
         this.fullName = fullName;
-        this.locationPattern = locationPattern;
+        // Route hash
         this.hash = hash;
+        // Route components for each page bind
         this.components = components;
 
+        // Parse the hash using the current router
         this.parse = function(hash) {
             router.parse(hash);
         }
 
+        // Interpolate the hash using configured routes hash and the specified values for the route variables
         this.interpolate = function(values) {
             return csRoute.interpolate(values);
         }
     }
 
+    // Creates a new routing config, must be used as parameter in $$.routing.configure
     this.routes = function() {
         return new RoutingConfig();
     }
 
+    // Configure routing system using the specified routing config (created by using $$.routes)
     this.configure = function(routingConfig) {
         // For each location configured
         for (var locationName in routingConfig.configuration) {
-            // Get this location config and the pattern
-            var locationConfig = routingConfig.configuration[locationName];
-            var locationPattern = routingConfig.configuration[locationName].pattern;
+            // Get this location and the specified config
+            var location = routingConfig.configuration[locationName];
 
+
+            // If there's a previouly configurated location with the same name get it, if not create a new one
             var dest;
-
             if (!self.configuration[locationName]) {
                 dest = self.configuration[locationName] = {};
             } else {
                 dest = self.configuration[locationName];
             }
 
+            // If there isn't a previously configured router in the location configuration create a new one
             if (!dest.router) {
                 // Create a new crossroads router
                 dest.router = crossroads.create();
                 dest.router.normalizeFn = crossroads.NORM_AS_OBJECT;
             }
 
-            if (!dest.pattern) {
-                dest.pattern = locationPattern;
-            }
+            // Adds the router to the location
+            location.router = dest.router;
 
-            // Adds the router to the location config
-            locationConfig.router = dest.router;
-
+            // If there isn't a previously configured routes object in the location configuration create a new one
             if (!dest.routes) {
                 dest.routes = {};
             }
 
+            if (!dest.config) {
+                dest.config = location.config;
+            }
+
             // For each hash configured for this location
-            for (var routeName in locationConfig.routes) {
+            for (var routeName in location.routes) {
+                // If the routeName is not the generic one, load the configuration (generic config is apllied to each detailed route)
                 if (routeName !== '') {
-                    var components;
+                    var components = {};
 
-                    // Gets the route configuration
-                    var routeConfig = locationConfig.routes[routeName];
-
-                    components = routeConfig.components;
-
-                    if ($$.isDefined(locationConfig.routes[''])) {
-                        $.extend(components, locationConfig.routes[''].components);
-                    } else if ($$.isDefined(dest.routes[''])) {
+                    // If there's a previously default route defined in configuration load the components with it
+                    if ($$.isDefined(dest.routes[''])) {
                         $.extend(components, dest.routes[''].components);
                     }
 
-                    // Creates the new route
-                    var newRoute = new Route(dest.router, routeConfig.name, routeConfig.fullName, locationPattern, routeConfig.hash, components, routeConfig.controller);
+                    // If there's a new default route defined in this location replace it (to have precedence over older one)
+                    if ($$.isDefined(location.routes[''])) {
+                        $.extend(components, location.routes[''].components);
+                    }
 
+                    // Gets the route configuration
+                    var routeConfig = location.routes[routeName];
+
+                    // Replace this route configuration to have precedence over all previous configuration
+                    $.extend(components, routeConfig.components);
+
+                    // Creates the new route
+                    var newRoute = new Route(dest.router, routeConfig.name, routeConfig.fullName, location.config, routeConfig.hash, components, routeConfig.controller);
+
+                    // Save it on the location routes
                     dest.routes[routeName] = newRoute;
                 }
             }
         }
     }
 
+
+
+    // Parses the specified route and location changing the current route
     this.parse = function(location, hash) {
         // If only one parameter is specified we assume that is the hash, and the location must be taken from the
         // window location object.
         if (!$$.isDefined(hash)) {
             hash = location;
-            location = window.location.pathname;
+            location = undefined;
         }
 
-        for (var locationName in self.configuration) {
-            var locationConfig = self.configuration[locationName];
+        for (var index in self.locationFinders) {
+            var finder = self.locationFinders[index];
 
-            var exp = RegExp(locationConfig.pattern);
-            if (location.match(exp)) {
-                return locationConfig.router.parse(hash);
-            }
+            finder(location, function(locationConfig) {
+                locationConfig.router.parse(hash);
+            });
         }
     }
 
@@ -248,38 +286,18 @@ function QuarkRouter() {
         }
     }
 
-    this.link = function(name, config, location) {
-        var route = self.getRoute(name);
-
-        if (!location) {
-            location = window.location.pathname;
-        }
-
-        if (route) {
-            var exp = RegExp(route.locationPattern);
-
-            if (location.match(exp)) {
-                return '#' + route.interpolate(config);
-            } else {
-                return location + '#' + route.interpolate(config);
-            }
-        }
-    }
-
     this.listLocation = function(location) {
-        if (!$$.isDefined(location)) {
-            location = window.location.pathname;
+        var locations = [];
+
+        for (var index in self.locationFinders) {
+            var finder = self.locationFinders[index];
+
+            finder(location, function(locationConfig) {
+                locations.push(locationConfig.routes);
+            });
         }
 
-        for (var locationName in self.configuration) {
-            var locationConfig = self.configuration[locationName];
-
-            var exp = RegExp(locationConfig.pattern);
-            if (location.match(exp)) {
-                return locationConfig.routes;
-            }
-        }
-
+        return locations;
     }
 
     this.activateHasher = function(callback) {
