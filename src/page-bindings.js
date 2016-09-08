5af082b02afae59ae2fa3e8132931746b9f254d0
admin <fllopez@bpba.com.ar>
2016-09-08 17:22:01 -0300

    Ajustes

--- src/page-bindings.js
+++ src/page-bindings.js
@@ -1,6 +1,3 @@
-// This binding works in conjunction with the routing system.
-// When configuring the routes you can define a page name and the component that must be shown when that route is matched.
-// The specified component is shown inside a component that has a page binding with the matching name.
 ko.bindingHandlers.page = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel, context) {
         // Page name on the route
@@ -13,17 +10,6 @@ ko.bindingHandlers.page = {
         }
 
         ko.applyBindingsToNode(element, { 'component': { 'name': current[value] } });
-
-/*
-        // Create an accessor for the component binding
-        var newAccesor = function () {
-            // Return the accesor for the component binding
-            return {
-                name: current[name]
-            }
-        };
-
-        return ko.bindingHandlers.component.init(element, newAccesor, allBindingsAccessor, viewModel, context);*/
     }
 }
 ko.virtualElements.allowedBindings.page = true;
